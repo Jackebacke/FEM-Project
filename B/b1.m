dcfc040ff84bee2e26ec011c2463fd3d89cb66a4
Jakob <jakob.nilsson.6654@student.uu.se>
2025-12-09 15:01:53 +0100

    eroor

--- B/b1.m
+++ B/b1.m
@@ -1,16 +1,32 @@
 clear; clc; close all;
+%% Geometry
 geometry = @circleg ;
-hmax = 1/5;
-[p , e , t] = initmesh(geometry ,'hmax' , hmax);
 
+%% Functions
 kappa = @(x,y) 1.0;
 f = @(x,y) 8.*pi^2 .* sin(2.*pi.*x) .* sin(2.*pi.*y);
 gamma = @(x,y) 1; % Large gamma to approximate Dirichlet BCs
 g = @(x,y) (2.*x.*pi.*cos(2.*pi.*x).*sin(2.*pi.*y) + 2.*y.*pi.*sin(2.*pi.*x).*cos(2.*pi.*y)) ./ (sqrt(x.^2 + y.^2)); % Neumann BC on boundary
 
-[A , R , b , r ] = assemble( p , e , t , kappa , f , g , gamma)
+function EnE = error(p, A,u_h)
+    u = @(x,y) sin(2.*pi.*x) .* sin(2.*pi.*y);
+    
+    err = u(p(1,:), p(2,:))' - u_h;
+    EnE = sqrt(err' * A * err);
 
+end
+
+H_MAX = [1/2, 1/4, 1/8, 1/16, 1/32];
+errors = [];
+
+for hmax = H_MAX
+[p , e , t] = initmesh(geometry ,'hmax' , hmax);
+[A , R , b , r ] = assemble( p , e , t , kappa , f , g , gamma);
 u_h = (A) \ (b + r);
+errors = [errors, error(p, A, u_h)];
+fprintf('hmax: %.5f, Error: %.5f\n', hmax, errors(end));
+end
+
 
 %% Plotting the solution
 figure;
@@ -21,3 +37,11 @@ ylabel('y');
 zlabel('u_h');
 axis equal;
 colorbar;
+
+figure;
+loglog(H_MAX, errors, '-o');
+hold on;
+xlabel('h_{max}');
+ylabel('Energy norm of error');
+title('Error vs. Mesh Size');
+grid on;
