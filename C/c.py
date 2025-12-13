41058c5f3ec59628e516070cdbfca4ca93eae55c
Jakob Nilsson <jakob.nilsson2002@gmail.com>
2025-12-13 15:46:20 +0100

    meshes

--- C/c.py
+++ C/c.py
@@ -1,7 +1,7 @@
 from dolfin import *
 
 # Create mesh and define function space
-mesh = Mesh("circle.xml.gz")
+mesh = Mesh("C/meshes/circle.xml.gz")
 
 # Construct the finite element space
 P1 = FiniteElement("Lagrange", mesh.ufl_cell() , 1)
@@ -23,14 +23,23 @@ l = 0.6
 m = 0.12
 
 # Class representing the intial conditions
-class InitialConditions ( UserExpression):
+class InitialConditions(UserExpression):
     def eval(self, values , x) :
-        values [ 0 ] = 0
-        values [ 1 ] = 4/15 - 2*10^{-7}*(x(0)-0.1*x(1)-350)((x(0)-0.1*x(1)-350))
-        values [ 2 ] = 22/45 -3*10^{-5}(x(0)-450)-1.2*10^{-4}(x(1)-15)
+        values[0] = 0
+        values[1] = 4/15 - 2*10**(-7)*(x[0]-0.1*x[1]-350)**2
+        values[2] = 22/45 - 3*10**(-5) * (x[0]-450) - 1.2*10**(-4) * (x[1]-15)
     def value_shape(self) :
         return (3 ,)
 
+# Define the nonlinear term
+def N(u_h):
+    u = u_h[0]
+    v = u_h[1]
+    w = u_h[2]
+    N1 = - alpha * u * (1 - u/(L_0 + l*v))
+    N2 = - beta * v * (1 - v) + v*w / (alpha + v + m * u)
+    N3 = gamma * w - zeta * v * w / (alpha + v + m * u)
+    return as_vector([N1 , N2 , N3])
 
 # Define initial condition
 indata = InitialConditions(degree = 2)
@@ -38,17 +47,24 @@ u0 = Function(W)
 u0 = interpolate(indata , W)
 
 # Create bilinear and linear forms
-a = ...
-L = ...
+psi = TestFunction(W)
+u = Function(W)
+D = as_matrix([[delta1 , 0 , 0] ,
+                   [0 , delta2 , 0] ,
+                   [0 , 0 , delta3]])
+a = dot(u, psi)*dx + dot(N(u), psi)*dx + inner(D*grad(u), grad(psi))*dx
+L = 0
 
 # Set an output file
-...
-# Set initial condition
-...
+file = File("solution.pvd")
+file << u
 
-# Time - stepping
-while t < T :
-    # assign u0
-    u0.assign( u)
-    ...
-    ...
+# # Set initial condition
+# ...
+
+# # Time - stepping
+# while t < T :
+#     # assign u0
+#     u0.assign(u)
+#     ...
+#     ...
